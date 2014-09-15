814384605704e4a8cef1caafc55f7d575e119b07
Ari Hausman-Cohen <ahausmancohe@cs.hmc.edu>
2014-09-15 00:41:07 -0700

    Finished list manipulation exercise 2

--- labs/src/main/scala/org/scalalabs/basic/lab02/ListManipulationExercise02.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab02/ListManipulationExercise02.scala
@@ -11,7 +11,7 @@ object ListManipulationExercise02 {
    * As usual, various ways exist: pattern matching, folding, ...
    */
   def maxElementInList(l: List[Int]): Int = {
-    error("fix me")
+    l.reduce{ (first, second) => if (first > second) first else second}
   }
 
   /**
@@ -19,7 +19,13 @@ object ListManipulationExercise02 {
    * of the two list
    */
   def sumOfTwo(l1: List[Int], l2: List[Int]): List[Int] = {
-    error("fix me")
+    (l1 zipAll (l2, 0, 0)).map{case (a,b) => a + b}
+    /* (l1, l2) match {
+      case (x::xs, y::ys) => (x+y) +: sumOfTwo(xs, ys)
+      case (x::xs, ys) => x +: sumOfTwo(xs, ys)
+      case (xs, y::ys) => y +: sumOfTwo(xs, ys)
+      case (_, _) => List.empty[Int]
+    } */
   }
 
   /**
@@ -27,7 +33,7 @@ object ListManipulationExercise02 {
    * method above
    */
   def sumOfMany(l: List[Int]*): List[Int] = {
-    error("fix me")
+    l.reduce(sumOfTwo(_, _))
   }
 
   case class Person(age: Int, firstName: String, lastName: String)
@@ -39,29 +45,31 @@ object ListManipulationExercise02 {
    * in a one-liner.
    */
   def separateTheYoungFromTheOld(persons: List[Person]): List[List[String]] = {
-    var youngins: ListBuffer[Person] = new ListBuffer[Person]()
-    var elders: ListBuffer[Person] = new ListBuffer[Person]()
-    var validYoungNames: ListBuffer[String] = new ListBuffer[String]()
-    var validOldNames: ListBuffer[String] = new ListBuffer[String]()
+    List(persons.filter{_.age < 18}, persons.filter{_.age >= 18}).map(_.sortBy(_.age).map(_.firstName))
+    
+    // var youngins: ListBuffer[Person] = new ListBuffer[Person]()
+    // var elders: ListBuffer[Person] = new ListBuffer[Person]()
+    // var validYoungNames: ListBuffer[String] = new ListBuffer[String]()
+    // var validOldNames: ListBuffer[String] = new ListBuffer[String]()
 
-    for (person <- persons) {
-        if (person.age < 18) {
-          youngins += person
-        } else {
-          elders += person
-        }
-    }
+    // for (person <- persons) {
+    //     if (person.age < 18) {
+    //       youngins += person
+    //     } else {
+    //       elders += person
+    //     }
+    // }
 
-    var sortedYoung = youngins.toList.sortBy(_.age)
-    var sortedOld = elders.toList.sortBy(_.age)
+    // var sortedYoung = youngins.toList.sortBy(_.age)
+    // var sortedOld = elders.toList.sortBy(_.age)
 
-    for (young <- sortedYoung) {
-      validYoungNames += young.firstName
-    }
-    for (old <- sortedOld) {
-      validOldNames += old.firstName
-    }
-    List(validYoungNames.toList, validOldNames.toList)
+    // for (young <- sortedYoung) {
+    //   validYoungNames += young.firstName
+    // }
+    // for (old <- sortedOld) {
+    //   validOldNames += old.firstName
+    // }
+    // List(validYoungNames.toList, validOldNames.toList)
   }
 
 }
